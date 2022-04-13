2093467a11a13d595903312f99d03267e9ea9c4c
Th1Alexandre <Th1Alexandre.dev@gmail.com>
2022-04-13 18:33:00 -0400

    rename class

--- criticas/apps.py
+++ criticas/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ReviewsAppConfig(AppConfig):
+class CriticasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'criticas'
