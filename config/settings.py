c0fbd68eadedfd905b4250645449889228e347e5
Th1Alexandre <Th1Alexandre.dev@gmail.com>
2022-04-24 01:05:44 -0400

    feat: set urls

--- config/settings.py
+++ config/settings.py
@@ -61,7 +61,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'criticas/templates'),
-            os.path.join(BASE_DIR, 'usuarios/templates')
+            os.path.join(BASE_DIR, 'usuarios/templates'),
+            os.path.join(BASE_DIR, 'templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
--- web_routes/views.py
+++ web_routes/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index/index.html')
+
+def review_(request):
+    return render(request, 'review_/review_.html')
+
+def review_edit(request):
+    return render(request, 'review_edit/review_edit.html')
+
+def review_new(request):
+    return render(request, 'review_new/review_new.html')
+
+def login(request):
+    return render(request, 'login/login.html')
+
+def signup(request):
+    return render(request, 'signup/signup.html')
