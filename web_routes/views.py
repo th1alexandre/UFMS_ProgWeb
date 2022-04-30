51d5e8abde061e17fed3c41d62bdb8606ce42ac6
Th1Alexandre <Th1Alexandre.dev@gmail.com>
2022-04-30 19:40:18 -0400

    feat: web_routes auth redirect

--- web_routes/views.py
+++ web_routes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
@@ -8,13 +8,27 @@ def review_(request):
     return render(request, 'review_/review_.html')
 
 def review_edit(request):
-    return render(request, 'review_edit/review_edit.html')
+    if _authenticated(request):
+        return render(request, 'review_edit/review_edit.html')
+    return redirect(sign_in)
 
 def review_new(request):
-    return render(request, 'review_new/review_new.html')
+    if _authenticated(request):
+        return render(request, 'review_new/review_new.html')
+    return redirect(sign_in)
 
 def sign_in(request):
+    if _authenticated(request):
+        return redirect(index)
     return render(request, 'sign_in/sign_in.html')
 
 def sign_up(request):
+    if _authenticated(request):
+        return redirect(index)
     return render(request, 'sign_up/sign_up.html')
+
+
+def _authenticated(request):
+    if request.user.is_authenticated:
+        return True
+    return False
