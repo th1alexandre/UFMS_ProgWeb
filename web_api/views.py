90ef5788d6018ff2ddc77237349fe3b37e49e19d
Th1Alexandre <Th1Alexandre.dev@gmail.com>
2022-04-30 19:50:23 -0400

    feat: web_api auth redirect

--- web_api/views.py
+++ web_api/views.py
@@ -6,51 +6,58 @@ from uuid import uuid4
 
 
 def login(request):
-    if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
+    if _authenticated(request):
+        return redirect(index)
+    else:
+        if request.method == 'POST':
+            email = request.POST['email']
+            password = request.POST['password']
 
-        user = User.objects.filter(email = email)
-        if user.exists():
-            username = user.values_list('username', flat=True).get()
-            user = auth.authenticate(request, username=username, password=password)
+            user = User.objects.filter(email = email)
+            if user.exists():
+                username = user.values_list('username', flat=True).get()
+                user = auth.authenticate(request, username=username, password=password)
 
-            if user is not None:
-                auth.login(request, user)
-                return redirect(index)
-        return redirect(sign_in)
+                if user is not None:
+                    auth.login(request, user)
+                    return redirect(index)
+            return redirect(sign_in)
 
 def logout(request):
-    auth.logout(request)
+    if _authenticated(request):
+        auth.logout(request)
     return redirect(index)
 
 def user_create(request):
-    if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        email = request.POST['email']
-        password_1 = request.POST['password_1']
-        password_2 = request.POST['password_2']
+    if _authenticated(request):
+        return redirect(index)
+    else:
+        if request.method == 'POST':
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            email = request.POST['email']
+            password_1 = request.POST['password_1']
+            password_2 = request.POST['password_2']
 
-        data = {
-            'first_name': first_name,
-            'last_name': last_name,
-            'email': email,
-            'password_1': password_1,
-            'password_2': password_2
-        }
+            data = {
+                'first_name': first_name,
+                'last_name': last_name,
+                'email': email,
+                'password_1': password_1,
+                'password_2': password_2
+            }
 
-        if _user_create_validate(data):
-            user = User.objects.create_user(
-                    username = uuid4(),
-                    first_name = data['first_name'],
-                    last_name = data['last_name'],
-                    email = data['email'],
-                    password = data['password_1']
-                )
-            user.save()
-            return redirect(sign_in)
-        return redirect(sign_up)
+            if _user_create_validate(data):
+                user = User.objects.create_user(
+                        username = uuid4(),
+                        first_name = data['first_name'],
+                        last_name = data['last_name'],
+                        email = data['email'],
+                        password = data['password_1']
+                    )
+                user.save()
+                return redirect(sign_in)
+            return redirect(sign_up)
 
 def user_delete(request):
     pass
@@ -58,6 +65,11 @@ def user_delete(request):
 def user_update(request):
     pass
 
+
+def _authenticated(request):
+    if request.user.is_authenticated:
+        return True
+    return False
 
 def _user_create_validate(args):
     if User.objects.filter(email = args['email']).exists():
@@ -67,7 +79,6 @@ def _user_create_validate(args):
         args['password_1'],
         args['password_2']
     )
-    
     return password
 
 def _user_create_validate_password(password_1, password_2):
