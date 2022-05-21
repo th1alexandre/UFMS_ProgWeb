bd16a2ccb63648d5e16a1ef65cc45c44cbb52fd9
Th1Alexandre <Th1Alexandre.dev@gmail.com>
2022-05-21 12:25:01 -0400

    feat: change frontend structure

--- web_routes/views.py
+++ web_routes/views.py
@@ -2,30 +2,30 @@ from django.shortcuts import render, redirect
 
 
 def index(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index.html')
 
 def review_(request):
-    return render(request, 'review_/review_.html')
+    return render(request, 'review_.html')
 
 def review_edit(request):
     if _authenticated(request):
-        return render(request, 'review_edit/review_edit.html')
+        return render(request, 'review_edit.html')
     return redirect(sign_in)
 
 def review_new(request):
     if _authenticated(request):
-        return render(request, 'review_new/review_new.html')
+        return render(request, 'review_new.html')
     return redirect(sign_in)
 
 def sign_in(request):
     if _authenticated(request):
         return redirect(index)
-    return render(request, 'sign_in/sign_in.html')
+    return render(request, 'sign_in.html')
 
 def sign_up(request):
     if _authenticated(request):
         return redirect(index)
-    return render(request, 'sign_up/sign_up.html')
+    return render(request, 'sign_up.html')
 
 
 def _authenticated(request):
